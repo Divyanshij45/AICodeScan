8b9ac3c2b30a897c09ebc64b62ee1089cbc83830
Divyanshi Joshi <joshidivyanshi065@gmail.com>
2025-07-25 06:48:17 +0530

    your commit message

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     
     setIsLoading(true)
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/ai/get-review`,code)
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/ai/get-review`,{code})
       console.log('Review response:', response.data)
       setReview(response.data)
     } catch (error) {
